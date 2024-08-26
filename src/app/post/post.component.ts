b19839cdacf0e0e7945ccf07214cfd81c85d79a3
mohammad2745 <mohammadfcc2745@gmail.com>
2024-08-27 01:13:46 +0600

    data pass from parent to child - input decorator

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-post',
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostComponent implements OnInit {
   title: string = 'post title';
-  
+  messagePost: string = 'message from post component';
+
+  @Input()
+  fromParent!: string;
+
   constructor() {}
 
   ngOnInit(): void {
