b43f7adbf22a7da9681f59e085b62ad97c90f412
mohammad2745 <mohammadfcc2745@gmail.com>
2024-08-27 00:55:42 +0600

    Data binding

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrl: './post.component.css'
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
+  title: string = 'post title';
+  
+  constructor() {}
 
+  ngOnInit(): void {
+
+  }
 }
